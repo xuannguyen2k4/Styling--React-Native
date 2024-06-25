722c3c7b5a9bd5f911247109ef57a6fe2fe6c5f7
Nguyen Xuan <nxuan2k4@gmail.com>
2024-06-25 14:04:57 +0700

    initiative

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View} from 'react-native';
 
-export default function App() {
+// Square Component
+const Square = ({ backgroundColor }) => {
+  return <View style={[styles.square, { backgroundColor }]} />;
+};
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Square backgroundColor="red" />
+      <Square backgroundColor="green" />
+      <Square backgroundColor="blue" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  square: {
+    width: 100,
+    height: 100,
+    margin: 10,
   },
 });
+
+export default App;
